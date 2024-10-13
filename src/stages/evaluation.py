be1a2e507de31d148597ed175e11acabc00b9705
Victor Bohorquez <vicbohoto@gmail.com>
2024-10-13 15:36:57 -0600

    Pipeline configured and running with dvc -VB

--- src/stages/evaluation.py
+++ src/stages/evaluation.py
@@ -8,16 +8,16 @@ import joblib
 def evaluation(path):
     with open(path) as con:
         config = yaml.safe_load(con)
-    evaluator = Models()
+    evaluator = Models(path)
     models = joblib.load(config['models']['path_models'])
     file = config['processed']['path_xtest']
     Xtest = pd.read_csv(file, sep=';')
     file = config['datasets']['path_ytest']
     ytest = pd.read_csv(file, sep=';')
-    models.evaluate(Xtest, ytest, models)
+    evaluator.evaluate(Xtest, ytest, models)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Models evaluation step.')
     parser.add_argument('--config', help='Path to the YAML configuration file.')
     args = parser.parse_args()
-    train(path=args.config)
+    evaluation(path=args.config)
