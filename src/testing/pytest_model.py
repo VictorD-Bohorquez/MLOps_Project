bc7108729b53a5ed3b93355fdfc4dc8e6fcdbb74
RubenGTM <122581960+RubenGTM@users.noreply.github.com>
2024-10-31 20:03:14 -0600

    Move tests within TestClass -RG

--- src/testing/pytest_model.py
+++ src/testing/pytest_model.py
@@ -7,16 +7,17 @@ import pandas as pd
 import joblib
 import pytest
 
-# This test is intended to validate Xtrain and ytrain have same num of rows
-def test_datasets_size():
-    xtrain_size, ytrain_size, xxx = train('params.yaml')
-    assert xtrain_size == ytrain_size
+class TestClass:
 
-# This test is intended to validate none column in Xtrain dataset is Object type
-def test_no_object_types():
-    xxx, yyy, column_types = train('params.yaml')
-    #object_type = 'object'
-    object_type = 'float64'
-    assert object_type  not in column_types.values
+    # This test is intended to validate Xtrain and ytrain have same num of rows
+    def test_datasets_size(self):
+        xtrain_size, ytrain_size, tmp = train('params.yaml')
+        assert xtrain_size == ytrain_size
+
+    # This test is intended to validate none column in Xtrain dataset is Object type
+    def test_no_object_types(self):
+        tmp1, tmp2, column_types = train('params.yaml')
+        object_type = 'object'
+        assert object_type  not in column_types.values
 
 
